4756216adf7a0ababa49512d5d12949ef927dd51
Pierre <pierre.yvenou@orange.fr>
2023-04-30 19:09:02 +0200

    Adding landmarks

--- app/src/main/java/com/example/atelierdevmobile2023pierrepablohugo2/MapActivity.kt
+++ app/src/main/java/com/example/atelierdevmobile2023pierrepablohugo2/MapActivity.kt
@@ -1,5 +1,4 @@
 package com.example.atelierdevmobile2023pierrepablohugo2
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,10 +8,23 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import okhttp3.*;
+import org.json.JSONObject
+import java.io.IOException
 
 internal class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
+    private val requestUrl="https://www.ugarit.online/epsi/stores.json"
+    private val request = Request.Builder()
+        .url(requestUrl)
+        .get()
+        .cacheControl(CacheControl.FORCE_NETWORK)
+        .build()
+
+    val pointsMap = arrayListOf<PointMap>()
+    val urlDefault ="https://img.freepik.com/photos-gratuite/rendu-3d-fond-espace-planetes-abstraites-nebuleuse_1048-12994.jpg?w=740&t=st=1673970224~exp=1673970824~hmac=5db29203b7bcddbecb50f50b6bab3920b729055ce5b87aaccc3260362a801f19"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +33,51 @@ internal class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body?.string()
+
+                if(data!=null) {
+                    val dataFetch = JSONObject(data)
+                    val arrayPoints = dataFetch.getJSONArray("stores")
+                    for (i in 0 until arrayPoints.length()) {
+                        val js = arrayPoints.getJSONObject(i)
+                        val point = PointMap(
+                            js.optInt("storeId", -1),
+                            js.optString("name", "not found"),
+                            js.optString("description", "not found"),
+                            js.optString("pictureStore", urlDefault),
+                            js.optString("address", "not found"),
+                            js.optString("zipcode", "00000"),
+                            js.optString("city", "not found"),
+                            js.optDouble("longitude", 0.0),
+                            js.optDouble("latitude", 0.0),
+
+                            )
+                        pointsMap.add(point)
+                    }
+
+                    println("Points map : "+pointsMap.size)
+                    runOnUiThread {
+                        for (point in pointsMap) {
+                            mMap.addMarker(MarkerOptions()
+                                .position(LatLng(point.longitude, point.latitude))
+                                .title(point.nom)
+                                .snippet(point.adresse+" "+point.codePostal+" "+point.ville))
+                        }
+                    }
+                }
+
+            }
+
+        })
+
     }
 
     /**
@@ -34,13 +91,9 @@ internal class MapActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val pointTest = LatLng( 48.0, 3.0)
 
-        // Add a marker in Sydney and move the camera
-        val paris = LatLng(48.0, 3.0)
-        mMap.addMarker(MarkerOptions()
-            .position(paris)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pointTest))
     }
 }
 
